27fa77356995118f95a3f53b5d5158d667327905
Jacopo <jacopo.carlini@gmail.com>
2022-07-18 15:36:31 +0200

    lint

--- Donation/__tests__/index.test.ts
+++ Donation/__tests__/index.test.ts
@@ -1,26 +1,24 @@
-import httpTrigger from '../index'
-import {
-    runStubFunctionFromBindings,
-    createHttpTrigger,
-} from 'stub-azure-function-context'
+// @ts-ignore
+import {createHttpTrigger, runStubFunctionFromBindings} from 'stub-azure-function-context';
+import httpTrigger from '../index';
 
 
 describe("feature donation", () => {
     it('success', async () => {
-        let req = {
+        const req = {
             properties: {
                 amount: 100,
                 description: "ukraine"
             }
         };
-        const result = await mockedRequestFactory(req)
+        const result = await mockedRequestFactory(req);
 
-        expect(result.res.status).toEqual(200)
-        const body = result.res.body
-        expect(body.paymentOption[0].amount).toEqual(req.properties.amount)
-        expect(body.paymentOption[0].description).toEqual(req.properties.description)
-        expect(result.res.headers['Content-Type']).toEqual("application/json")
-    })
+        expect(result.res.status).toEqual(200);
+        const body = result.res.body;
+        expect(body.paymentOption[0].amount).toEqual(req.properties.amount);
+        expect(body.paymentOption[0].description).toEqual(req.properties.description);
+        expect(result.res.headers['Content-Type']).toEqual("application/json");
+    });
 
 
 });
@@ -46,5 +44,5 @@ async function mockedRequestFactory(body: any) {
             {type: 'http', name: 'result', direction: 'out'},
         ],
         new Date(),
-    )
+    );
 }
--- Donation/index.ts
+++ Donation/index.ts
@@ -1,41 +1,51 @@
-import {AzureFunction, Context, HttpRequest} from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 
-
-
-// POST http://localhost:7071/api/Donation
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.', req.body);
-    const properties = req.body.properties
-
-    const body = {
-        paymentOption: [
-            {
-                iuv: null,
-                amount: properties.amount,
-                description: properties.description,
-                isPartialPayment: false,
-                dueDate: addDays(new Date(), Number(process.env['ADD_DUE_DATE_DAYS'])),
-                retentionDate: addDays(new Date(), Number(process.env['ADD_RETENTION_DATE_DAYS'])),
-                fee: 0,
-                transfer: [{
-                    amount: properties.amount
-                }]
-            }
-        ]
-    }
-    context.res = {
-        status: 200,
-        body: body,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-
+const addDays = (date: Date, days: number): Date => {
+  date.setDate(date.getDate() + days);
+  return date;
 };
 
-function addDays(date: Date, days: number): Date {
-    date.setDate(date.getDate() + days);
-    return date;
-}
+// POST http://localhost:7071/api/Donation
+const httpTrigger: AzureFunction = async (
+  context: Context,
+  req: HttpRequest
+): Promise<void> => {
+  context.log("HTTP trigger function processed a request.", req.body);
+
+  // TODO check input
+
+  const properties = req.body.properties;
+
+  const body = {
+    paymentOption: [
+      {
+        amount: properties.amount,
+        description: properties.description,
+        dueDate: addDays(new Date(), Number(process.env.ADD_DUE_DATE_DAYS)),
+        fee: 0,
+        isPartialPayment: false,
+        iuv: null,
+        retentionDate: addDays(
+          new Date(),
+          Number(process.env.ADD_RETENTION_DATE_DAYS)
+        ),
+        transfer: [
+          {
+            amount: properties.amount
+          }
+        ]
+      }
+    ]
+  };
+
+  // eslint-disable-next-line functional/immutable-data
+  context.res = {
+    body,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    status: 200
+  };
+};
 
 export default httpTrigger;
